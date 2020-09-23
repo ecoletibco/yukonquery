5662424bec262c098d9149dc6a2d59542a5c3fea
ECole <eddie.cole.nh@gmail.com>
2020-09-23 11:52:46 -0400

    Implemented Yukon Connect

--- activity.go
+++ activity.go
@@ -1,11 +1,23 @@
 package yukonquery
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"time"
 
 	"github.com/project-flogo/core/activity"
 	"github.com/project-flogo/core/data/metadata"
 	"github.com/project-flogo/core/support/log"
+)
+
+const (
+	MethodGET  = "GET"
+	MethodPOST = "POST"
+	MethodPUT  = "PUT"
 )
 
 type YukonConnection struct {
@@ -27,6 +39,7 @@ type Query struct {
 }
 
 type Activity struct {
+	client          *http.Client
 	connectionId    string
 	connectionToken string
 	query           Query
@@ -55,17 +68,23 @@ func New(ctx activity.InitContext) (activity.Activity, error) {
 		return nil, err
 	}
 
+	client, err := getHttpClient(20)
+	if err != nil {
+		return nil, err
+	}
+
+	connectionId, connectionToken, err := connect(client, s)
+	if err != nil {
+		return nil, err
+	}
+
 	query, err := parseQuery(s.Query)
 	if err != nil {
 		return nil, err
 	}
 
-	connectionId, connectionToken, err := connect(s)
-	if err != nil {
-		return nil, err
-	}
-
 	act := &Activity{
+		client:          &client,
 		connectionId:    connectionId,
 		connectionToken: connectionToken,
 		query:           query,
@@ -109,14 +128,76 @@ func parseQuery(queryString string) (Query, error) {
 
 	var queryObj = Query{}
 
-	if false {
-		return queryObj, fmt.Errorf("only select statement is supported")
+	if queryString == "" {
+		return queryObj, fmt.Errorf("'query' is required")
 	}
 
 	return queryObj, nil
 }
 
-func connect(s *Settings) (string, string, error) {
+func connect(client http.Client, s *Settings) (string, string, error) {
+
+	if s.URL == "" {
+		return "", "", fmt.Errorf("'url' is required")
+	}
+
+	if s.UcsConnectionId != "" {
+		connectionId, connectionToken, err := connectViaUCS(client, s)
+		if err != nil {
+			return "", "", err
+		}
+		return connectionId, connectionToken, nil
+	} else {
+		connectionId, connectionToken, err := connectNative(client, s)
+		if err != nil {
+			return "", "", err
+		}
+		return connectionId, connectionToken, nil
+	}
+}
+
+func connectNative(client http.Client, s *Settings) (string, string, error) {
+
+	if s.ConnectorName == "" {
+		return "", "", fmt.Errorf("'connectorName' is required")
+	}
+
+	yukonConn := &YukonConnection{
+		ConnectorName:   s.ConnectorName,
+		ConnectionProps: s.ConnectorProps,
+	}
+
+	baseUrl := s.URL
+
+	uri := baseUrl + "/connections"
+
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/json"
+
+	reqBodyJSON, err := json.Marshal(yukonConn)
+	if err != nil {
+		return "", "", err
+	}
+	reqBody := bytes.NewBuffer([]byte(reqBodyJSON))
+
+	resp, err := getRestResponse(client, MethodPOST, uri, headers, reqBody)
+	if err != nil {
+		return "", "", err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&yukonConn)
+	if err != nil {
+		return "", "", err
+	}
+
+	return yukonConn.Id, yukonConn.Token, nil
+}
+
+func connectViaUCS(client http.Client, s *Settings) (string, string, error) {
+
+	if s.UcsConnectionToken == "" {
+		return "", "", fmt.Errorf("'ucsConnectionToken' is required")
+	}
 
 	var connectionId = ""
 	var connectionToken = ""
@@ -127,4 +208,92 @@ func connect(s *Settings) (string, string, error) {
 func getResults(queryResult interface{}) ([][]interface{}, error) {
 
 	return nil, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+// Utils
+
+func getHttpClient(timeout int) (http.Client, error) {
+
+	client := &http.Client{}
+
+	httpTransportSettings := &http.Transport{}
+
+	if timeout > 0 {
+		httpTransportSettings.ResponseHeaderTimeout = time.Second * time.Duration(timeout)
+	}
+
+	client.Transport = httpTransportSettings
+
+	return *client, nil
+}
+
+func getRestResponse(client http.Client, method string, uri string, headers map[string]string, reqBody io.Reader) (*http.Response, error) {
+
+	req, err := http.NewRequest(method, uri, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		return resp, errors.New("Bad Response: " + resp.Status)
+	}
+
+	if resp == nil {
+		return resp, errors.New("Empty Response")
+	}
+
+	return resp, nil
+}
+
+func getBodyAsText(respBody io.ReadCloser) string {
+
+	defer func() {
+		if respBody != nil {
+			_ = respBody.Close()
+		}
+	}()
+
+	var response = ""
+
+	if respBody != nil {
+		b := new(bytes.Buffer)
+		b.ReadFrom(respBody)
+		response = b.String()
+	}
+
+	return response
+}
+
+func getBodyAsJSON(respBody io.ReadCloser) (interface{}, error) {
+
+	defer func() {
+		if respBody != nil {
+			_ = respBody.Close()
+		}
+	}()
+
+	d := json.NewDecoder(respBody)
+	d.UseNumber()
+	var response interface{}
+	err := d.Decode(&response)
+	if err != nil {
+		switch {
+		case err == io.EOF:
+			return nil, nil
+		default:
+			return nil, err
+		}
+	}
+
+	return response, nil
 }
--- activity_test.go
+++ activity_test.go
@@ -4,8 +4,24 @@ import (
 	"testing"
 
 	"github.com/project-flogo/core/activity"
+	"github.com/project-flogo/core/data/mapper"
+	"github.com/project-flogo/core/data/resolve"
+	"github.com/project-flogo/core/support/test"
 	"github.com/stretchr/testify/assert"
 )
+
+const (
+	TestUrl                = "https://localhost:44346/api"
+	TestUcsConnectionId    = ""
+	TestUcsConnectionToken = ""
+	TestConnectorName      = "Benchmark"
+	TestQuery              = "select * from entity2"
+)
+
+var TestConnectorProps = map[string]string{
+	"Username": "ecole@tibco.com",
+	"Password": "XXXXX",
+}
 
 func TestRegister(t *testing.T) {
 
@@ -13,4 +29,171 @@ func TestRegister(t *testing.T) {
 	act := activity.Get(ref)
 
 	assert.NotNil(t, act)
+}
+
+func TestSettings(t *testing.T) {
+
+	// valid settings
+	settings := &Settings{
+		URL:                TestUrl,
+		UcsConnectionId:    TestUcsConnectionId,
+		UcsConnectionToken: TestUcsConnectionToken,
+		ConnectorName:      TestConnectorName,
+		ConnectorProps:     TestConnectorProps,
+		Query:              TestQuery,
+	}
+
+	iCtx := test.NewActivityInitContext(settings, nil)
+	_, err := New(iCtx)
+	assert.Nil(t, err)
+
+	// No URL
+	settings = &Settings{
+		URL:                "",
+		UcsConnectionId:    TestUcsConnectionId,
+		UcsConnectionToken: TestUcsConnectionToken,
+		ConnectorName:      TestConnectorName,
+		ConnectorProps:     TestConnectorProps,
+		Query:              TestQuery,
+	}
+
+	iCtx = test.NewActivityInitContext(settings, nil)
+	_, err = New(iCtx)
+	assert.NotNil(t, err)
+
+	// Bad URL
+	settings = &Settings{
+		URL:                "https://tibco.com",
+		UcsConnectionId:    TestUcsConnectionId,
+		UcsConnectionToken: TestUcsConnectionToken,
+		ConnectorName:      TestConnectorName,
+		ConnectorProps:     TestConnectorProps,
+		Query:              TestQuery,
+	}
+
+	iCtx = test.NewActivityInitContext(settings, nil)
+	_, err = New(iCtx)
+	assert.NotNil(t, err)
+
+	// No Query
+	settings = &Settings{
+		URL:                TestUrl,
+		UcsConnectionId:    TestUcsConnectionId,
+		UcsConnectionToken: TestUcsConnectionToken,
+		ConnectorName:      TestConnectorName,
+		ConnectorProps:     TestConnectorProps,
+		Query:              "",
+	}
+
+	iCtx = test.NewActivityInitContext(settings, nil)
+	_, err = New(iCtx)
+	assert.NotNil(t, err)
+
+	// No user
+	tmpConnectorProps := TestConnectorProps
+	tmpConnectorProps["Username"] = ""
+	settings = &Settings{
+		URL:                TestUrl,
+		UcsConnectionId:    TestUcsConnectionId,
+		UcsConnectionToken: TestUcsConnectionToken,
+		ConnectorName:      TestConnectorName,
+		ConnectorProps:     tmpConnectorProps,
+		Query:              TestQuery,
+	}
+
+	iCtx = test.NewActivityInitContext(settings, nil)
+	_, err = New(iCtx)
+	assert.NotNil(t, err)
+
+	// No password
+	tmpConnectorProps = TestConnectorProps
+	tmpConnectorProps["Password"] = ""
+	settings = &Settings{
+		URL:                TestUrl,
+		UcsConnectionId:    TestUcsConnectionId,
+		UcsConnectionToken: TestUcsConnectionToken,
+		ConnectorName:      TestConnectorName,
+		ConnectorProps:     tmpConnectorProps,
+		Query:              TestQuery,
+	}
+
+	iCtx = test.NewActivityInitContext(settings, nil)
+	_, err = New(iCtx)
+	assert.NotNil(t, err)
+
+	/*
+
+		Benchmark does not suppoty this test
+
+		// Bad creds
+		tmpConnectorProps = TestConnectorProps
+		tmpConnectorProps["Username"] = "BadUsername"
+		tmpConnectorProps["Password"] = "BadPassword"
+		settings = &Settings{
+			URL:                TestUrl,
+			UcsConnectionId:    TestUcsConnectionId,
+			UcsConnectionToken: TestUcsConnectionToken,
+			ConnectorName:      TestConnectorName,
+			ConnectorProps:     TestConnectorProps,
+			Query:              TestQuery,
+		}
+
+		iCtx = test.NewActivityInitContext(settings, nil)
+		_, err = New(iCtx)
+		assert.NotNil(t, err)
+	*/
+}
+
+func TestEvalSimpleSelect(t *testing.T) {
+
+	settings := &Settings{
+		URL:                TestUrl,
+		UcsConnectionId:    TestUcsConnectionId,
+		UcsConnectionToken: TestUcsConnectionToken,
+		ConnectorName:      TestConnectorName,
+		ConnectorProps:     TestConnectorProps,
+		Query:              TestQuery,
+	}
+
+	mf := mapper.NewFactory(resolve.GetBasicResolver())
+	iCtx := test.NewActivityInitContext(settings, mf)
+	act, err := New(iCtx)
+	assert.Nil(t, err)
+
+	tc := test.NewActivityContext(act.Metadata())
+
+	//setup attrs
+	tc.SetInput("uuid", "BPId")
+
+	//eval
+	act.Eval(tc)
+	assert.NotNil(t, tc.GetOutput("uuid"))
+	assert.NotNil(t, tc.GetOutput("data"))
+}
+
+func TestEvalBadQuery(t *testing.T) {
+
+	settings := &Settings{
+		URL:                TestUrl,
+		UcsConnectionId:    TestUcsConnectionId,
+		UcsConnectionToken: TestUcsConnectionToken,
+		ConnectorName:      TestConnectorName,
+		ConnectorProps:     TestConnectorProps,
+		Query:              "BadQuery",
+	}
+
+	mf := mapper.NewFactory(resolve.GetBasicResolver())
+	iCtx := test.NewActivityInitContext(settings, mf)
+	act, err := New(iCtx)
+	assert.Nil(t, err)
+
+	tc := test.NewActivityContext(act.Metadata())
+
+	//setup attrs
+	tc.SetInput("uuid", "bad1e6e5-f722-45e9-af49-e5380cf14003")
+
+	//eval
+	act.Eval(tc)
+	assert.NotNil(t, tc.GetOutput("uuid"))
+	assert.NotNil(t, tc.GetOutput("data"))
 }
